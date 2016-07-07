2709577ab47e040732cb385333ac58441e60785c
Jessica <jkustra92@gmail.com>
2016-07-07 12:08:57 -0400

    adding s3 encryption

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   validates :image, presence: true
   belongs_to :user
   has_attached_file :image,
-                    :styles => {:medium => "300x300>"}
+                    :styles => {:medium => "300x300>"},
+                    :file, s3_permissions: :private,
+                         s3_server_side_encryption: "AES256"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
