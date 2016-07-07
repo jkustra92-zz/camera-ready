1a35c40be0b79e8da55452a94e46e4fc730b1059
Jessica <jkustra92@gmail.com>
2016-07-07 11:38:04 -0400

    adding additional security on authenticated routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: :new, :create, :show, :destroy
   before_action :set_user, only: [:new, :create, :show, :destroy]
   before_action :set_post, only: [:show, :destroy]
 
