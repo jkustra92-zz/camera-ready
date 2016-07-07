ac27fd3938e7f6a22eb3e36a1fa7f6cdf182b574
Jessica <jkustra92@gmail.com>
2016-07-07 13:04:09 -0400

    think i finally figured out encryption

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
   belongs_to :user
-  has_attached_file :image, s3_permissions: :private, s3_server_side_encryption: "AES256"
-                    :styles => {:medium => "300x300>"},
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image,
+                    :styles => {:medium => "300x300>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, s3_permissions: :private,
+                         s3_server_side_encryption: "AES256"
 end
