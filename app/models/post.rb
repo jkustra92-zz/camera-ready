772cc6d6f81dae522e05ca2736f0740d32cbbaec
Jessica <jkustra92@gmail.com>
2016-07-07 12:55:16 -0400

    aaaand i'm breaking stuff again

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
   belongs_to :user
-  has_attached_file :image, s3_permissions: :private, s3_server_side_encryption: "AES256",
-                    :styles => {:medium => "300x300>"}
+  has_attached_file :image,
+                    :styles => {:medium => "300x300>"},
+                    :file => {s3_permissions: :private,
+                         s3_server_side_encryption: "AES256"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
