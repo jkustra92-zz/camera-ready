823986e7717efd40fd5dd0e8226cd613a54ba45e
Jessica <jkustra92@gmail.com>
2016-07-07 12:50:51 -0400

    encryption? do you work?

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
   belongs_to :user
-  has_attached_file :file, s3_permissions: :private,
-                         s3_headers: { "x-amz-server-side-encryption" => "AES256" },
-                    :image,
+  has_attached_file :image, s3_permissions: :private, s3_server_side_encryption: "AES256",
                     :styles => {:medium => "300x300>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
