4291cea97cf451861ff0ee04a68e535b0c0faf9f
Jessica <jkustra92@gmail.com>
2016-07-07 12:12:38 -0400

    fixing typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image,
                     :styles => {:medium => "300x300>"},
-                    :file, s3_permissions: :private,
-                         s3_server_side_encryption: "AES256"
+                    :file => {s3_permissions: :private,
+                         s3_server_side_encryption: "AES256"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
