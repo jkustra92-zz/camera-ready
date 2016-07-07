20a15cc921da48a4d5d7eda61450f678993e94d6
Jessica <jkustra92@gmail.com>
2016-07-07 16:57:48 -0400

    fixing thumbnail stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image,
                     :styles => {:medium => "300x300>"}
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, s3_permissions: :private,
-                         s3_server_side_encryption: "AES256"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
